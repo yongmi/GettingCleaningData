75f7d9d3e6edff09cf7b4500f88b0fdcfb8dc4f4
yellow green <yongmi@gmail.com>
2014-12-21 14:43:07 -0500

    Initial run_analysis.R file

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+# Getting and Cleaning Data
+# Course Project
+# 2014-12-18
+
+setwd("~/Documents/Coursera/DataScienceTrack/GettingCleaningData/CourseProject")
+
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set 
+#    with the average of each variable for each activity and each subject.
+
+library(dplyr)
+library(stringr)
+features<-read.table("UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+actnames<-read.table("UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE, col.names=c("activity", "activity.NAME"))
+colnames(features) <- c("fid", "fname")
+myfeatures<-filter(features, str_detect(fname,"mean\\(|std\\("))
+
+subject <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names=c("SID"))
+trainset <- read.table("UCI HAR Dataset/train/X_train.txt")
+acts <- read.table("UCI HAR Dataset/train/y_train.txt", col.names=c("activity"))
+
+trainset2 <- trainset[myfeatures$fid]
+traincombo <- cbind(subject, acts, trainset2)
+traincombo2 <- inner_join(traincombo, actnames)
+
+subject <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names=c("SID"))
+testset <- read.table("UCI HAR Dataset/test/X_test.txt")
+acts <- read.table("UCI HAR Dataset/test/y_test.txt", col.names=c("activity"))
+
+testset2 <- testset[myfeatures$fid]
+testcombo <- cbind(subject, acts, testset2)
+testcombo2 <- inner_join(testcombo, actnames)
+
+combo = rbind(traincombo2, testcombo2)
+# writeltable(row.name=FALSE)
